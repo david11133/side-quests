7a0267b6ced80b2a7e16071a2ca7ec4eaad0fa13
david11133 <davidnady4yad@gmail.com>
2025-12-09 16:18:36 +0200

    Update image links from the new OneDrive links

--- Orbital Welding Data Collection/MD to CSV/main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_table("table.md", sep="|", engine="python", skipinitialspace=True)
-df = df.dropna(axis=1, how="all")  # remove empty columns caused by leading/trailing pipes
-df.to_csv("table.csv", index=False)
-df.to_excel("table.xlsx", index=False)
